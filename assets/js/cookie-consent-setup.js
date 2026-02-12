b6545e840e16a7db5e1372930d77347e6c4e1dc5
zihanliu987 <zihan.liu987@gmail.com>
2026-02-12 11:30:58 +0000

    Deploying to gh-pages from @ zihanliu987/zihan.liu987.github.io@093034f5f4931b89eadddbf95d54952f5e1d6ccc 🚀

--- assets/js/cookie-consent-setup.js
+++ assets/js/cookie-consent-setup.js
@@ -96,7 +96,7 @@ function initializeCookieConsent() {
               },
               {
                 title: 'More information',
-                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://yourusername.github.io/#contact">contact us</a>.'
+                description: 'For any queries in relation to our policy on cookies and your choices, please <a class="cc-link" href="https://zihanliu987.github.io/#contact">contact us</a>.'
               }
             ]
           }
--- assets/js/giscus-setup.js
+++ assets/js/giscus-setup.js
@@ -18,7 +18,7 @@ function determineGiscusTheme() {
 
   let giscusAttributes = {
     src: "https://giscus.app/client.js",
-    "data-repo": "zihanliu987/zihan.liu.github.io",
+    "data-repo": "zihanliu987/zihan.liu987.github.io",
     "data-repo-id": "",
     "data-category": "Comments",
     "data-category-id": "",
